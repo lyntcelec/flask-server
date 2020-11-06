0dadc460b965975e7a2db4654ab7839d85992a4e
Ly Nguyen <lyntc.elec@gmail.com>
2020-11-06 16:52:04 +0700

    - Using Flask app context

--- app/__init__.py
+++ app/__init__.py
@@ -22,39 +22,39 @@ migrate = Migrate()
 def create_app():
     global blacklist
     app = Flask(__name__)
+    with app.app_context():
+        print ("Environment", config.settings.BaseConfig.ENV )
+        if config.settings.BaseConfig.ENV == "production":
+            app.config.from_object(config.settings.ProductionConfig)
+        else:
+            app.config.from_object(config.settings.DevelopmentConfig)
+        
+        app.secret_key = app.config['JWT_SECRET_KEY']
+        jwt.init_app(app)
 
-    print ("Environment", config.settings.BaseConfig.ENV )
-    if config.settings.BaseConfig.ENV == "production":
-        app.config.from_object(config.settings.ProductionConfig)
-    else:
-        app.config.from_object(config.settings.DevelopmentConfig)
-    
-    app.secret_key = app.config['JWT_SECRET_KEY']
-    jwt.init_app(app)
+        # Token blacklist
+        @jwt.token_in_blacklist_loader
+        def check_if_token_in_blacklist(decrypted_token):
+            jti = decrypted_token['jti']
+            return jti in jwt_blacklist
+        
+        # initialize SQLAlchemy
+        db.init_app(app)
 
-    # Token blacklist
-    @jwt.token_in_blacklist_loader
-    def check_if_token_in_blacklist(decrypted_token):
-        jti = decrypted_token['jti']
-        return jti in jwt_blacklist
-    
-    # initialize SQLAlchemy
-    db.init_app(app)
+        login_manager.init_app(app)
+        login_manager.login_message = "You must be logged in to access this page."
+        login_manager.login_view = "auth.login"
 
-    login_manager.init_app(app)
-    login_manager.login_message = "You must be logged in to access this page."
-    login_manager.login_view = "auth.login"
+        # initialize migrations
+        migrate.init_app(app, db)
+        from app import model
+        from app import api
 
-    # initialize migrations
-    migrate.init_app(app, db)
-    from app import model
-    from app import api
+        api.api_restful.init_app(app)
 
-    api.api_restful.init_app(app)
-
-    # define hello world page
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+        # define hello world page
+        @app.route('/')
+        def hello_world():
+            return 'Hello, World!'
 
     return app
